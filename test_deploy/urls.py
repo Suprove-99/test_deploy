cabd18bbdc4e245f4b40485ff6ab949923b2a3f0
Suprove Chandra Sarkar <suprove.c.sarkar@gmail.com>
2021-08-03 17:31:24 +0600

    Update urls.py

--- test_deploy/urls.py
+++ test_deploy/urls.py
@@ -17,10 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-#
-from django.conf.urls import url
-from django.views.static import serve
-from django.conf import settings  # ??
 
 urlpatterns = [
     path('admin/', admin.site.urls),
